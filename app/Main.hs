b35ace5c0194acf451e241a056d435b3d55f0fe4
Glauber Modolo Cabral <glauber.sp@gmail.com>
2015-10-01 22:54:33 -0300

    Versão 0.1.0.0.
    Arquivo funcionando aceitando uma lista de arquivos de disciplinas e
    uma lista de arquivos de alunos.

--- app/Main.hs
+++ app/Main.hs
@@ -1,153 +1,40 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 -- | Main entry point to the application.
 module Main where
 
-import Lib
 import System.Environment
-import Control.Applicative
-import qualified Data.ByteString.Lazy as BL
-import qualified Data.Vector as V
-import Data.Csv
-import qualified Data.Text.Lazy as T
-import qualified Data.Char as C
+import Options.Applicative
+import Transformações
 
--- | Modelo para CSV de entrada de disciplinas
-data DisciplinasEntrada = DisciplinasEntrada
-    { anoD :: !Int
-    , semestreD :: !Int
-    , disciplinaD :: !T.Text
-    , descricaoD :: !T.Text
-    , departamentoD :: !T.Text
-    , turmaD :: !Int
-    , cargaHorariaD :: !Int
-    , professoresD :: !T.Text
-    } deriving (Show)
+data Opções = Opções
+  { disciplinas :: [String]
+  , alunos :: [String] }
 
--- | Modelo para CSV de saída de disciplinas
-data DisciplinasSaida = DisciplinasSaida
-    { shortnameD :: !T.Text
-    , fullnameD :: !T.Text
-    , categoryD :: !T.Text
-    , visibleD :: !T.Text
-    , formatD :: !T.Text
-    , enrolment_1D :: !T.Text
-    , enrolment_1_roleD :: !T.Text
-    } deriving (Show)
+opções :: Parser Opções
+opções = Opções
+     <$> many (strOption
+         ( short 'd'
+        <> metavar "DISCIPLINA.csv"
+        <> help "Arquivo CSV de Disciplinas de Departamento." ))
+     <*> many (strOption
+         ( short 'a'
+        <> metavar "ALUNO.csv"
+        <> help "Arquivo CSV de Alunos (diário de classe)." ))
 
-{-
-ANO SEMESTRE    COD DISCIPLINA  MATRICULA   NOME    CURSO   TURMA   EMAIL
-2013    2   CEA439  11.2.8065   ADRIANO ROMERO FRANCISCO    SJM 11  drico.rfc@gmail.com
--}
--- | Modelo para CSV de entrada de alunos
-data AlunosEntrada = AlunosEntrada
-    { ano :: !Int
-    , semestre :: !Int
-    , codigo :: !T.Text
-    , matricula :: !T.Text
-    , nome :: !T.Text
-    , curso :: !T.Text
-    , turma :: !Int
-    , emailE :: !T.Text
-    } deriving (Show)
+transformar :: Opções -> IO ()
+transformar (Opções [] []) = return ()
+transformar (Opções [] a) = mapM_ transformaAlunos a
+transformar (Opções d []) = mapM_ transformaDisciplinas d
+transformar (Opções d a) = do
+    mapM_ transformaDisciplinas d
+    mapM_ transformaAlunos a
 
-{-
-username    password    firstname   lastname    email   cohort1
-sjm1128065  sjm1128065  Adriano     Romero Francisco    drico.rfc@gmail.com 2013-2_CEA439-11
--}
--- | Modelo para CSV de saída de alunos
-data AlunosSaida = AlunosSaida
-    { username :: !T.Text
-    , password :: !T.Text
-    , firstname :: !T.Text
-    , lastname :: !T.Text
-    , email :: !T.Text
-    , cohort1 :: !T.Text
-    } deriving (Show)
-
-disciplinasSaidaHeader :: Header
-disciplinasSaidaHeader = V.fromList ["shortname", "fullname", "category", "visible", "format", "enrolment_1", "enrolment_1_role"]
-
-instance FromNamedRecord DisciplinasEntrada where
-    parseNamedRecord r = DisciplinasEntrada <$> r .: "ANO" <*> r .: "SEMESTRE" <*> r .: "DISCIPLINA" <*> r .: "DESCRICAO" <*> r .: "DEPARTAMENTO" <*> r .: "TURMA" <*> r .: "CARGA HORARIA" <*> r .: "PROFESSORES"
-
-instance ToNamedRecord DisciplinasSaida where
-    toNamedRecord (DisciplinasSaida s f c v ft e1 e1r) =
-        namedRecord ["shortname" .= s, "fullname" .= f, "category" .= c, "visible" .= v, "format" .= ft, "enrolment_1" .= e1, "enrolment_1_role" .= e1r]
-
-alunosSaidaHeader :: Header
-alunosSaidaHeader = V.fromList ["username", "password", "firstname", "lastname", "email", "cohort1"]
-
-instance FromNamedRecord AlunosEntrada where
-    parseNamedRecord r = AlunosEntrada <$> r .: "ANO" <*> r .: "SEMESTRE" <*> r .: "COD DISCIPLINA" <*> r .: "MATRICULA" <*> r .: "NOME" <*> r .: "CURSO" <*> r .: "TURMA" <*> r .: "EMAIL"
-
-instance ToNamedRecord AlunosSaida where
-    toNamedRecord (AlunosSaida u p f l e c) =
-        namedRecord ["username" .= u, "password" .= p, "firstname" .= f, "lastname" .= l, "email" .= e, "cohort1" .= c]
-
-opcoesDec :: DecodeOptions
-opcoesDec = defaultDecodeOptions {
-                decDelimiter = fromIntegral (C.ord ';')
-            }
-
-opcoesEnc :: EncodeOptions
-opcoesEnc = defaultEncodeOptions {
-                encDelimiter = fromIntegral (C.ord ';')
-            }
-
--- | The main entry point.
+-- | The main function.
 main :: IO ()
-main = do
-    (disciplinasFile:alunosFile:_) <- getArgs
-    -- Trata arquivo de disciplinas
-    disciplinasCSVData <- BL.readFile disciplinasFile
-    case decodeByNameWith opcoesDec disciplinasCSVData of
-        Left err -> putStrLn err
-        Right (_, vector) -> do
-            disciplinasSaidas <- V.forM vector $ \ entr -> do
-                let
-                    shortN = T.pack (show $ anoD entr)
-                                `T.append` "-"
-                                `T.append` T.pack (show $ semestreD entr)
-                                `T.append` "_"
-                                `T.append` (disciplinaD entr)
-                                `T.append` "-"
-                                `T.append` T.pack (show $ turmaD entr)
-                    fullN = disciplinaD entr
-                                `T.append` "-"
-                                `T.append` T.pack (show $ turmaD entr)
-                                `T.append` " - "
-                                `T.append` T.toTitle (descricaoD entr)
-                                `T.append` " - "
-                                `T.append` T.toTitle (professoresD entr)
-                    cat = departamentoD entr
-                            `T.append` " / "
-                            `T.append` T.pack (show $ anoD entr)
-                            `T.append` "-"
-                            `T.append` T.pack (show $ semestreD entr)
-                    vis = T.pack (show 1)
-                    fmt = "topics"
-                    enrol_1 = "cohort"
-                    enrol_1_role = "student"
-                return $ DisciplinasSaida shortN fullN cat vis fmt enrol_1 enrol_1_role
-            --print (V.toList AlunosSaidas)
-            let outputFile = "DisciplinasSaida.csv"
-            BL.writeFile outputFile $ encodeByNameWith opcoesEnc disciplinasSaidaHeader (V.toList disciplinasSaidas)
-    -- Trata arquivo de alunos
-    alunosCSVData <- BL.readFile alunosFile
-    case decodeByNameWith opcoesDec alunosCSVData of
-        Left err -> putStrLn err
-        Right (_, vector) -> do
-            alunosSaidas <- V.forM vector $ \ entr -> do
-                let user = T.toLower (curso entr `T.append` T.filter (/= '.') (matricula entr))
-                    pwd = user
-                    nm = T.splitOn " " $ nome entr
-                    fname = T.toTitle $ T.concat $ take 1 nm
-                    lname = T.toTitle $ T.unwords $ drop 1 nm
-                    mail = emailE entr
-                    c1 = T.pack (show $ ano entr) `T.append` "-" `T.append` T.pack (show $ semestre entr) `T.append` "_" `T.append` codigo entr `T.append` "-" `T.append` T.pack (show $ turma entr)
-                    sd = AlunosSaida user pwd fname lname mail c1
-                return sd
-            --print (V.toList AlunosSaidas)
-            let outputFile = "AlunosSaida.csv" --T.unpack (cohort1 ((V.toList alunosSaidas)!!0)) ++ ".csv"
-            BL.writeFile outputFile $ encodeByNameWith opcoesEnc alunosSaidaHeader (V.toList alunosSaidas)
+main = execParser opts >>= transformar
+  where
+    opts = info (helper <*> opções)
+            ( fullDesc
+            <> progDesc "Este programa prepara os arquivos CSV do MinhaUFOP para serem utilizados na carga de arquivos do Moodle para criação de cursos e alunos. O arquivo DISCIPLINA.csv é o CSV de disciplinas de um departamento. O arquivo ALUNO.csv é o arquivo do diário de classe para uma dada turma. É possível repetir as opções para diversos arquivos."
+            <> header "moodleUFOP - Transforma arquivos CSV do MinhaUFOP para carga no Moodle."
+            )
+
---
+++ app/Transformações.hs
@@ -0,0 +1,158 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+-- | Módulo para transformar arquivos CSV do MinhaUFOP.
+module Transformações
+    ( transformaDisciplinas
+    , transformaAlunos
+    ) where
+
+import Control.Applicative
+import Prelude hiding (readFile, writeFile)
+import Data.ByteString.Lazy (readFile, writeFile)
+import qualified Data.Vector as V
+import Data.Csv
+import qualified Data.Text.Lazy as T
+import qualified Data.Char as C
+
+
+-- | Modelo para CSV de elemVetorada de disciplinas
+data DisciplinasEntrada = DisciplinasEntrada
+    { anoD :: !Int
+    , semestreD :: !Int
+    , disciplinaD :: !T.Text
+    , descricaoD :: !T.Text
+    , departamentoD :: !T.Text
+    , turmaD :: !Int
+    , cargaHorariaD :: !Int
+    , professoresD :: !T.Text
+    } deriving (Show)
+
+-- | Modelo para CSV de saída de disciplinas
+data DisciplinasSaida = DisciplinasSaida
+    { shortnameD :: !T.Text
+    , fullnameD :: !T.Text
+    , categoryD :: !T.Text
+    , visibleD :: !T.Text
+    , formatD :: !T.Text
+    , enrolment_1D :: !T.Text
+    , enrolment_1_roleD :: !T.Text
+    } deriving (Show)
+
+{-
+ANO SEMESTRE    COD DISCIPLINA  MATRICULA   NOME    CURSO   TURMA   EMAIL
+2013    2   CEA439  11.2.8065   ADRIANO ROMERO FRANCISCO    SJM 11  drico.rfc@gmail.com
+-}
+-- | Modelo para CSV de elemVetorada de alunos
+data AlunosEntrada = AlunosEntrada
+    { ano :: !Int
+    , semestre :: !Int
+    , disciplina :: !T.Text
+    , matricula :: !T.Text
+    , nome :: !T.Text
+    , curso :: !T.Text
+    , turma :: !Int
+    , emailE :: !T.Text
+    } deriving (Show)
+
+{-
+username    password    firstname   lastname    email   cohort1
+sjm1128065  sjm1128065  Adriano     Romero Francisco    drico.rfc@gmail.com 2013-2_CEA439-11
+-}
+-- | Modelo para CSV de saída de alunos
+data AlunosSaida = AlunosSaida
+    { username :: !T.Text
+    , password :: !T.Text
+    , firstname :: !T.Text
+    , lastname :: !T.Text
+    , email :: !T.Text
+    , cohort1 :: !T.Text
+    } deriving (Show)
+
+disciplinasSaidaHeader :: Header
+disciplinasSaidaHeader = V.fromList ["shortname", "fullname", "category", "visible", "format", "enrolment_1", "enrolment_1_role"]
+
+instance FromNamedRecord DisciplinasEntrada where
+    parseNamedRecord r = DisciplinasEntrada <$> r .: "ANO" <*> r .: "SEMESTRE" <*> r .: "DISCIPLINA" <*> r .: "DESCRICAO" <*> r .: "DEPARTAMENTO" <*> r .: "TURMA" <*> r .: "CARGA HORARIA" <*> r .: "PROFESSORES"
+
+instance ToNamedRecord DisciplinasSaida where
+    toNamedRecord (DisciplinasSaida s f c v ft e1 e1r) =
+        namedRecord ["shortname" .= s, "fullname" .= f, "category" .= c, "visible" .= v, "format" .= ft, "enrolment_1" .= e1, "enrolment_1_role" .= e1r]
+
+alunosSaidaHeader :: Header
+alunosSaidaHeader = V.fromList ["username", "password", "firstname", "lastname", "email", "cohort1"]
+
+instance FromNamedRecord AlunosEntrada where
+    parseNamedRecord r = AlunosEntrada <$> r .: "ANO" <*> r .: "SEMESTRE" <*> r .: "COD DISCIPLINA" <*> r .: "MATRICULA" <*> r .: "NOME" <*> r .: "CURSO" <*> r .: "TURMA" <*> r .: "EMAIL"
+
+instance ToNamedRecord AlunosSaida where
+    toNamedRecord (AlunosSaida u p f l e c) =
+        namedRecord ["username" .= u, "password" .= p, "firstname" .= f, "lastname" .= l, "email" .= e, "cohort1" .= c]
+
+opcoesDec :: DecodeOptions
+opcoesDec = defaultDecodeOptions {
+                decDelimiter = fromIntegral (C.ord ';')
+            }
+
+opcoesEnc :: EncodeOptions
+opcoesEnc = defaultEncodeOptions {
+                encDelimiter = fromIntegral (C.ord ';')
+            }
+
+-- | Trata arquivo de disciplinas por departamento.
+transformaDisciplinas :: FilePath -> IO ()
+transformaDisciplinas disciplinasFile = do
+    disciplinasCSVData <- readFile disciplinasFile
+    case decodeByNameWith opcoesDec disciplinasCSVData of
+        Left err -> putStrLn err
+        Right (_, vector) -> do
+            disciplinasSaidas <- V.forM vector $ \ elemVetor -> do
+                let
+                    shortN = T.pack (show $ anoD elemVetor)
+                                `T.append` "-"
+                                `T.append` T.pack (show $ semestreD elemVetor)
+                                `T.append` "_"
+                                `T.append` (disciplinaD elemVetor)
+                                `T.append` "-"
+                                `T.append` T.pack (show $ turmaD elemVetor)
+                    fullN = disciplinaD elemVetor
+                                `T.append` "-"
+                                `T.append` T.pack (show $ turmaD elemVetor)
+                                `T.append` " - "
+                                `T.append` T.toTitle (descricaoD elemVetor)
+                                `T.append` " - "
+                                `T.append` T.toTitle (professoresD elemVetor)
+                    cat = departamentoD elemVetor
+                            `T.append` " / "
+                            `T.append` T.pack (show $ anoD elemVetor)
+                            `T.append` "-"
+                            `T.append` T.pack (show $ semestreD elemVetor)
+                    vis = T.pack (show 1)
+                    fmt = "topics"
+                    enrol_1 = "cohort"
+                    enrol_1_role = "student"
+                return $ DisciplinasSaida shortN fullN cat vis fmt enrol_1 enrol_1_role
+            --print (V.toList AlunosSaidas)
+            let outputFile = T.replace "/" "_" $ T.filter (/= ' ') (categoryD ((V.toList disciplinasSaidas)!!0)) `T.append` ".csv"
+            writeFile (T.unpack outputFile) $ encodeByNameWith opcoesEnc disciplinasSaidaHeader (V.toList disciplinasSaidas)
+
+-- | Trata arquivo de alunos (diário de classe).
+transformaAlunos :: FilePath -> IO ()
+transformaAlunos alunosFile = do
+    alunosCSVData <- readFile alunosFile
+    case decodeByNameWith opcoesDec alunosCSVData of
+        Left err -> putStrLn err
+        Right (_, vector) -> do
+            alunosSaidas <- V.forM vector $ \ elemVetor -> do
+                let user = T.toLower (curso elemVetor `T.append` T.filter (/= '.') (matricula elemVetor))
+                    pwd = user
+                    nm = T.splitOn " " $ nome elemVetor
+                    fname = T.toTitle $ T.concat $ take 1 nm
+                    lname = T.toTitle $ T.unwords $ drop 1 nm
+                    mail = emailE elemVetor
+                    chrt1 = T.pack (show $ ano elemVetor) `T.append` "-" `T.append` T.pack (show $ semestre elemVetor) `T.append` "_" `T.append` disciplina elemVetor `T.append` "-" `T.append` T.pack (show $ turma elemVetor)
+                    sd = AlunosSaida user pwd fname lname mail chrt1
+                return sd
+            --print (V.toList AlunosSaidas)
+            let outputFile = (cohort1 ((V.toList alunosSaidas)!!0)) `T.append` ".csv"
+            writeFile (T.unpack outputFile) $ encodeByNameWith opcoesEnc alunosSaidaHeader (V.toList alunosSaidas)
+
